8c08d02425c45664976eafda5d8fc80248c3e085
Denys Asauliak <denoming@gmail.com>
2022-08-03 20:20:02 +0300

    remove http client example

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 #include <freertos/task.h>
 
 #include "WiFi.hpp"
-#include "HttpClient.hpp"
-#include "HttpChunkSender.hpp"
 
 #include <esp_err.h>
 #include <esp_log.h>
@@ -18,49 +16,8 @@ setup()
     ESP_LOGI(TAG, "SetUp WiFi");
     wifi.setUp();
 
-    HttpClient client("192.168.1.20", 8000, "/speech");
-    client.setHeader(Http::Field::Expect, "100-continue");
-    client.setHeader(Http::Field::UserAgent, "J.A.R.V.I.S Agent");
-    client.setMethod(HTTP_METHOD_POST);
-    client.setTimeout(8000);
-    client.connect(HttpClient::kChunkConnection);
-
-    if (client.connected()) {
-        int statusCode{0};
-
-        int attempt = 5;
-        while (attempt-- > 0) {
-            client.fetchHeaders();
-            statusCode = client.getStatusCode();
-            if (statusCode == 100) {
-                break;
-            }
-            ESP_LOGE(TAG, "Attempt to fetch again: %d", attempt);
-            vTaskDelay(pdMS_TO_TICKS(200));
-        }
-
-        if (statusCode == 100) {
-            const char* text = "Some long long long text";
-            HttpChunkSender sender{client};
-            if (!sender.send(text)) {
-                ESP_LOGE(TAG, "Failed to send chunk");
-            }
-            if (!sender.finalize()) {
-                ESP_LOGE(TAG, "Failed to finalize");
-            }
-        } else {
-            ESP_LOGE(TAG, "Invalid status code: %d", statusCode);
-        }
-    } else {
-        ESP_LOGI(TAG, "Failed to connect");
-    }
-
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    client.disconnect();
-    client.cleanup();
-
     ESP_LOGI(TAG, "Waiting...");
-    vTaskDelay(pdMS_TO_TICKS(1000));
+    vTaskDelay(pdMS_TO_TICKS(3000));
 
     ESP_LOGI(TAG, "TearDown WiFi");
     wifi.tearDown();
